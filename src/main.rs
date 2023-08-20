27fcb53c7b5caf972b26c58309acd5bd4b86faf2
0xshodan <hngdean@gmail.com>
2023-08-20 11:25:36 +0300

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-use axum::{routing::get, Router};
-use dotenvy::dotenv;
-use dotenvy_macro::dotenv;
-
-#[tokio::main]
-async fn main() {
-    dotenv().ok();
-    let db_url = dotenv!("DATABASE_URL");
+fn main() {
     println!("Hello, world!");
 }
